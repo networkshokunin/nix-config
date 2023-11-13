78618683d7b98392318d1572c4a7fd8302b482cf
David Lim <david@networkshokunin.com>
2023-11-13 22:58:02 +0800

    Adding default.nix for hosts

---
+++ hosts/common/global/default.nix
@@ -0,0 +1,40 @@
+{ inputs, outputs, ... }: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ./lldpd.nix
+    ./locale.nix
+    ./openssh.nix
+    ./packages.nix
+    ./users.nix
+    ./zfs.nix
+    ./zsh.nix
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+  #home-manager.extraSpecialArgs = { inherit inputs outputs; };
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  #networking.domain = "s5.com";
+
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
+}
