adb4ce72be12d859c45521e37061407c8b14089a
David Lim <david@networkshokunin.com>
2023-11-19 10:56:37 +0800

    fix typo and set to path of fonts

--- hosts/common/global/jetbrains-mono.nix
+++ hosts/common/global/jetbrains-mono.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: 
 {
-  fonts.fonts = with pkgs; = [
+  fonts.fonts = with pkgs; [
       jetbrains-mono
     ];
 }
--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,12 +1,13 @@
-{ pkgs, lib, ... }: 
+{ pkgs, lib, ... }:
 {
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     earlySetup = true;
     keyMap = "us";
     packages = with pkgs; [ jetbrains-mono ];
-    font = "${pkgs.jetbrains-mono}/share/consolefonts/JetBrainsMono-Regular.ttf";   
+    font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    #use fc-list to list the fonts installed and then set the font
   };
-  
+
   time.timeZone = "Asia/Singapore";
 }
