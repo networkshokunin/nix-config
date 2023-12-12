a756041b41e6223d6bf7fe7f71be742ad9f2f70b
David Lim <david@networkshokunin.com>
2023-12-12 22:42:11 +0800

    set zone to master

--- hosts/common/optional/bind.nix
+++ hosts/common/optional/bind.nix
@@ -12,21 +12,24 @@
      };
     '';
     zones = {
-      "example.com".extraConfig = "
-        $TTL 2d    ; 172800 secs default TTL for zone
-        $ORIGIN example.com.
-        @             IN      SOA   ns1.example.com. hostmaster.example.com. (
-                                2003080800 ; se = serial number
-                                12h        ; ref = refresh
-                                15m        ; ret = update retry
-                                3w         ; ex = expiry
-                                3h         ; min = minimum
-                                )
-                      IN      NS      ns1.example.com.
-        joe           IN      A       192.168.254.3
-        www           IN      CNAME   joe
-      ";
+      "example.com" = {
+        master = true;
+        extraConfig = ''
+          $TTL 2d    ; 172800 secs default TTL for zone
+          $ORIGIN example.com.
+          @             IN      SOA   ns1.example.com. hostmaster.example.com. (
+                                  2003080800 ; se = serial number
+                                  12h        ; ref = refresh
+                                  15m        ; ret = update retry
+                                  3w         ; ex = expiry
+                                  3h         ; min = minimum
+                                  )
+                        IN      NS      ns1.example.com.
+          joe           IN      A       192.168.254.3
+          www           IN      CNAME   joe
+          '';
+      };
+
      };  
     }; 
- };
 }
