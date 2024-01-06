b3efe086d454f3ff235ade58953ae38de5ba4e4e
David Lim <david@networkshokunin.com>
2024-01-06 23:50:11 +0800

    proper formatting

--- hosts/common/optional/chrony-server.nix
+++ hosts/common/optional/chrony-server.nix
@@ -1,7 +1,7 @@
 {
   services.chrony = {
     enable = true;
-    server = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org"];
+    server = ["0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" "3.pool.ntp.org"];
     }; 
-  networking.firewall.allowed = ["123" , "udp"];
+  networking.firewall.allowedUDPPorts = [ 53 ];
 }
