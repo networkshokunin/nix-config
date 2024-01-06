58d557719039de99236d4e22f615281d27ad1f3b
David Lim <david@networkshokunin.com>
2024-01-07 07:44:44 +0800

    typo

--- hosts/common/optional/chrony-server.nix
+++ hosts/common/optional/chrony-server.nix
@@ -1,7 +1,7 @@
 {
   services.chrony = {
     enable = true;
-    server = ["0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" "3.pool.ntp.org"];
+    servers = ["0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" "3.pool.ntp.org"];
     }; 
   networking.firewall.allowedUDPPorts = [ 123 ];
 }
